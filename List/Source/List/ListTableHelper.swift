ef838e09d8699d8ba9921c940f907fd26d09619d
Mehdi Badjian <mmbadjian@gmail.com>
2018-06-29 10:57:37 +0800

    Added ListTableViewCell and updated ListTableHelper for datasource

--- List/Source/List/ListTableHelper.swift
+++ List/Source/List/ListTableHelper.swift
@@ -11,3 +11,27 @@ import UIKit
 class ListTableHelper: NSObject {
 
 }
+
+extension ListTableHelper : UITableViewDelegate {
+  
+}
+
+extension ListTableHelper : UITableViewDataSource {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    var cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.identifier, for: indexPath) as? ListTableViewCell
+    if (cell == nil) {
+      cell = ListTableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: ListTableViewCell.identifier)
+    }
+    return cell!
+  }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 0
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return 0
+  }
+}
---
+++ List/Source/List/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  List
+//
+//  Created by MBadj on 29/06/2018.
+//  Copyright © 2018 Mehdi Badjian. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
