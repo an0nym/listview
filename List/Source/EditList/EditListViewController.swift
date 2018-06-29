16624c8f6b17b59c4ed217c624718af688c63538
Mehdi Badjian <mmbadjian@gmail.com>
2018-06-29 10:14:00 +0800

    Restructore project folders, Added list and edit view controller

---
+++ List/Source/EditList/EditListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EditListViewController.swift
+//  List
+//
+//  Created by MBadj on 29/06/2018.
+//  Copyright © 2018 Mehdi Badjian. All rights reserved.
+//
+
+import UIKit
+
+class EditListViewController: UIViewController {
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
+}
---
+++ List/Source/List/ListTableHelper.swift
@@ -0,0 +1,13 @@
+//
+//  ListTableHelper.swift
+//  List
+//
+//  Created by MBadj on 29/06/2018.
+//  Copyright © 2018 Mehdi Badjian. All rights reserved.
+//
+
+import UIKit
+
+class ListTableHelper: NSObject {
+
+}
