108b3b97968ddef50d78366850f270d87ed2c618
Mehdi Badjian <mmbadjian@gmail.com>
2018-06-29 11:02:37 +0800

    Updated ListViewController

--- List/Source/List/ListViewController.swift
+++ List/Source/List/ListViewController.swift
@@ -9,14 +9,32 @@
 import UIKit
 import SnapKit
 
-class ListViewController: UITableViewController {
+class ListViewController: UIViewController {
 
+  var tableView = UITableView()
+  var listTableHelper : ListTableHelper
+
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    self.listTableHelper = ListTableHelper.init()
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
       super.viewDidLoad()
   }
   
   func configureView () {
-    
+    self.tableView = UITableView.init(frame: .zero)
+    self.tableView.delegate = self.listTableHelper
+    self.tableView.dataSource = self.listTableHelper
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+    tableView.register(ListTableViewCell.self, forCellReuseIdentifier: ListTableViewCell.identifier)
+    self.view.addSubview(self.tableView)
+    self.layoutConstraints()
   }
   
   func layoutConstraints () {
